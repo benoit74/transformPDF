f5250ad71f07794168c91991119895b460d52479
Benoit Beraud <bberaud@octo.com>
2016-07-15 08:58:07 +0200

    Fix to resources (missing S3 Bucket policy for SES receipt rule to be accepted)

--- functions/main/handler.js
+++ functions/main/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports.handler = function(event, context, cb) {
+  console.log('Received event:', JSON.stringify(event));
   return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
   });
