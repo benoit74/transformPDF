91e46220f08f235212c10f5d35a29d5355ea9a99
Benoit Beraud <bberaud@octo.com>
2016-07-15 10:11:49 +0200

    Call the event processor

--- functions/main/handler.js
+++ functions/main/handler.js
@@ -1,8 +1,15 @@
 'use strict';
 
+
+var eventProcessor = require("../../lib/eventProcessor");
+
 module.exports.handler = function(event, context, cb) {
-  console.log('Received event:', JSON.stringify(event));
-  return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
+//  console.log('Received event:', JSON.stringify(event));
+  var proc = new eventProcessor.EventProcessor();
+  proc.processS3IncomingMail(event)
+   .then(function() {
+     return cb(null, {
+       message: 'Go Serverless! Your Lambda function executed successfully!'
+     });
+   });
 };
